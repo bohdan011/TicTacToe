ea30703e83558e2d3a02559a756ccde2a3b2a1c9
Bohdan <bogdan.dp.16@gmail.com>
2021-12-20 18:45:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const WINNING_COMB = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const WinningMsgEl = document.getElementById('winningMessage');
+const restartBtn = document.getElementById('restartBtn');
+const winningMsgTextEl = document.querySelector('[data-message]');
+let circleTurn;
+
+startGame();
+restartBtn.addEventListener('click', startGame)
+
+function swapTurn() {
+	circleTurn = !circleTurn;
+}
+
+function checkWin(currentClass) {
+	return WINNING_COMB.some(combination => {
+		return combination.every(index => {
+			return cellElements[index].classList.contains(currentClass);
+		});
+	});
+}
+
+function placeMark(cell, currentClass) {
+	cell.classList.add(currentClass);
+}
+
+function isDraw() {
+	return [...cellElements].every(cell => {
+		return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+	})
+}
+
+function setBoardHoverClass() {
+	board.classList.remove(X_CLASS);
+	board.classList.remove(CIRCLE_CLASS);
+	if (circleTurn) {
+		board.classList.add(CIRCLE_CLASS)
+	} else {
+		board.classList.add(X_CLASS)
+	}
+}
+
+function endGame(draw) {
+	if (draw) {
+		winningMsgTextEl.innerHTML = 'Draw!';
+	} else {
+		winningMsgTextEl.innerHTML = `${circleTurn ? '0\'s' : 'X\'s'}`;
+	}
+	WinningMsgEl.classList.add('show');
+}
+
+function handleClick(e) {
+	const cell = e.target;
+	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+	placeMark(cell, currentClass);
+	if (checkWin(currentClass)) {
+		endGame(false);
+	} else if (isDraw()) {
+		endGame(true)
+	} else {
+		swapTurn();
+		setBoardHoverClass();
+	}
+}
+
+function startGame() {
+	circleTurn = false;
+	cellElements.forEach(cell => {
+		cell.classList.remove(X_CLASS);
+		cell.classList.remove(CIRCLE_CLASS);
+		cell.removeEventListener('click', handleClick);
+		cell.addEventListener('click', handleClick);
+	});
+	setBoardHoverClass();
+	WinningMsgEl.classList.remove('show');
+}
